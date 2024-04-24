c13671d9627a8f958dd5a1b8d1cf31b253ff9205
Nicolas Perez <93753717+NickPPerez@users.noreply.github.com>
2024-04-24 06:40:51 -0400

    Fixed enemy drop rate and spawning

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -18,31 +18,13 @@ public class EnemyController : MonoBehaviour
     private GameObject[] enemies;
     [SerializeField] GameObject[] items;
 
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        pc = playerTransform.GetComponent<PlayerController>();
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
-        Spawn();
-    }
-
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
-
-        if (distanceToPlayer <= attackRange)
-        {
-            // attack the player
-            //pc.TakeDamage(10);
-        }
-        else
-        {
-            agent.SetDestination(goal.position);
-        }
         if (current_health <= 0) {
+            int rand = Random.Range(0, enemies.Length);
+            GameObject item = items[rand];
             Destroy(gameObject);
+            Instantiate(item, transform.position, Quaternion.identity);
         }
     }
 
@@ -51,19 +33,6 @@ public class EnemyController : MonoBehaviour
             current_health -= 10;
             Debug.Log("Enemy Health: " + current_health);
         }
-    }
-
-    private void Spawn()
-    {
-        /* Spawn enemies in random positions */
-
-        float randomX = Random.Range(-5f, 5f);
-        float randomY = Random.Range(-5f,5f);
-        Vector2 spawnPosition = new Vector2(randomX, randomY);
-        /* Spawn up to 5 enemies */
-        GameObject enemy = enemies[Random.Range(0,5)];
- 
-        Instantiate(enemy, spawnPosition, Quaternion.identity);
     }
 
 }
