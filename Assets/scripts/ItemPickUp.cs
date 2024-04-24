6de699438f7a9c0efd65a74e5f21b35dbdf2a1d5
Nicolas Perez <93753717+NickPPerez@users.noreply.github.com>
2024-04-23 21:11:35 -0400

    Item Pick Up Script
    Items get picked up

---
+++ Assets/scripts/ItemPickUp.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            Destroy(gameObject);
+        }
+    }
+}
